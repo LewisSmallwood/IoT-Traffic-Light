e24740ce57b1da8c61f5a0da7950f2de1711776f
lewissmallwood <lewis@smallwood.it>
2019-01-10 19:53:12 +0000

    Turn on TV via Wake on LAN when sat in sofa.

--- main.ino
+++ main.ino
@@ -3,25 +3,32 @@
 // ESP266 WiFi and HTTP libraries.
 #include <ESP8266WiFi.h>
 #include <ESP8266WiFiMulti.h>
-
-#include <WiFiClient.h>
+#include <WiFiUDP.h>
 
 ESP8266WiFiMulti WiFiMulti;
+WiFiUDP UDP;
 
 // WiFi config
-const WIFI_SSID = "";
-const WIFI_PASSWORD = "";
+const char* WIFI_SSID = "";
+const char* WIFI_PASSWORD = "";
 
-int threshold = 200;  // Resistance threshold triggle value.
-                      // 200 calibrated to detect me, but not light shopping/animals/etc.
+int threshold = 200;      // Resistance threshold triggle value.
+                          // 200 calibrated to detect me, but not light shopping/animals/etc.
                       
-int knockSensor = 0;  // Pin A0
-int value = 0;        // Resistance value of pressure pad.
+int pressureSensor = 0;   // Pin A0
+int value = 0;            // Resistance value of pressure pad.
 
 // The last 1 second of sensed resistance values.
 // Takes 1 poll every 200ms and averages
 // over 1 second to avoid false positives.
 int history[5] = {0, 0, 0, 0, 0};
+
+boolean isSitting = false;
+
+IPAddress broadcast_ip(255,255,255,255);                 // BROADCAST IP
+
+// Target MAC address of device to switch on when sat in sofa.
+byte tvMACAddress[] = { 0x80, 0xC7, 0x55, 0x38, 0x3D, 0x73 };
 
 void setup() {
   Serial.begin(115200);
@@ -29,17 +36,13 @@ void setup() {
   // Use the onboard LED to indicate if someone is sat in the sofa or not.
   pinMode(LED_BUILTIN, OUTPUT);
 
-  Serial.println();
-  Serial.println();
-  Serial.println();
-
   // Wait 3 seconds for the WiFi card to initialise.
   for (uint8_t t = 4; t > 0; t--) {
       Serial.printf("[SETUP] WAIT %d...\n", t);
       Serial.flush();
       delay(1000);
   }
-
+  
   // Connect to the WiFi using credentials.
   WiFi.mode(WIFI_STA);
   WiFiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
@@ -48,34 +51,57 @@ void setup() {
   if (WiFi.status() == WL_CONNECTED) {
       WiFi.hostname("Sofa");
   }
+
+  // Start the UDP client.
+  UDP.begin(9);
 }
 
-// Once setup begin...
+// Once setup has finished, begin...
 void loop() {
   // If connected to WiFi, begin sensing.
   if ((WiFiMulti.run() == WL_CONNECTED)) {
       WiFi.hostname("Sofa");
-      
-      value = analogRead(knockSensor);
+
+      // Get the current pressure plate 
+      value = analogRead(pressureSensor);
       addToHistory(history, value);
-  
+
+      // Check if the average pressure pad resistance
+      // in the last second exceeds the threshold.
       if (average(history, 5) < threshold) {
-          digitalWrite(LED_BUILTIN, HIGH);
+          setSittingState(true);
       } else {
-          digitalWrite(LED_BUILTIN, LOW);
+          setSittingState(false);
       }
   }
 
-  delay(200); // Wait for 200ms before looping again.
+  delay(200);
 }
 
-// Gets the average of all numbers within an array.
-float average (int * array, int len)  // assuming array is int.
-{
-    long sum = 0L ;  // sum will be larger than an item, long for safety.
-    
+// Called when the sitting state changes.
+void onChangedSittingState() {
+    if (isSitting) {
+        digitalWrite(LED_BUILTIN, HIGH);
+              
+    } else {
+        digitalWrite(LED_BUILTIN, LOW);
+        SendWakeOnLAN(tvMACAddress, sizeof tvMACAddress);
+    }
+}
+
+// Set the sitting state.
+void setSittingState(boolean newState) {
+    if (newState != isSitting) {
+        isSitting = newState;
+        onChangedSittingState();
+    }
+}
+
+// Gets the average of all numbers within an int array.
+float average (int * array, int len) {
+    long sum = 0L;
     for (int i = 0 ; i < len ; i++) {
-        sum += array [i] ;
+        sum += array[i];
     }
     
     return  ((float) sum) / len;
@@ -90,3 +116,18 @@ void addToHistory(int * array, int value) {
     array[0] = value;
 }
 
+// Send a Wake on LAN request to MAC address
+void SendWakeOnLAN(byte * mac, size_t mac_size) {
+    byte preamble[] = {0xFF, 0xFF, 0xFF, 0xFF, 0xFF, 0xFF};
+    byte i;
+    
+    UDP.beginPacket(broadcast_ip, 9);
+  
+    UDP.write(preamble, sizeof preamble);
+    
+    for (i = 0; i < 16; i++) {
+        UDP.write(mac, mac_size);
+    }
+    
+    UDP.endPacket();
+}
